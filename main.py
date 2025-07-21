5a16e5bd46ab083eedb62f3048c876236c6665d2
Тимур <tim.gai2009@yandex.ru>
2025-07-21 20:48:15 +0500

    Исправления багов

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QApplication, QMainWindow, QMenuBar, QMenu
-import config, functionality
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMenuBar, QMenu, QMessageBox, QFileDialog
+from settings_gui import SettingsGUI
+import config
 
 
 class Window(QMainWindow):
@@ -16,7 +17,7 @@ class Window(QMainWindow):
 
         # Поле для названия текста
         self.text_title = QtWidgets.QLineEdit(self)
-        self.text_title.setGeometry(10, 30, 780, 51)
+        self.text_title.setGeometry(10, 30, 780, 71)
         self.text_title.setAutoFillBackground(False)
         self.text_title.setText('.txt')
         self.text_title.setCursorPosition(0)
@@ -25,7 +26,7 @@ class Window(QMainWindow):
                                         "border: None")
         # Поле для ввода текста
         self.text_editor = QtWidgets.QTextEdit(self)
-        self.text_editor.setGeometry(10, 90, 780, 461)
+        self.text_editor.setGeometry(10, 110, 780, 441)
         self.text_editor.setAutoFillBackground(False)
         self.text_editor.setStyleSheet(f"font: {config.default_font_size} \"{config.default_font}\";\n"
                                         f"background-color: {config.default_background_color};\n"
@@ -34,7 +35,7 @@ class Window(QMainWindow):
         self.save_button = QtWidgets.QPushButton(self)
         self.save_button.setGeometry(QtCore.QRect(690, 560, 91, 28))
         self.save_button.setText('Сохранить')
-        self.save_button.clicked.connect(functionality.save_file)
+        self.save_button.clicked.connect(self.action_clicked)
         # Создание меню-бара
         self.create_menu_bar()
 
@@ -55,13 +56,47 @@ class Window(QMainWindow):
     def action_clicked(self):
         action = self.sender().text()
         if action == 'Открыть':
-            functionality.open_file(self)
+            file_name = QFileDialog.getOpenFileName(self, "Открыть файл", f"{config.default_folder}",
+                                            "Текстовые документы(*.txt)")[0]
+            try:
+                if file_name != '':
+                    self.text_title.setText(file_name)
+                    with open(file_name, 'r', encoding='utf-8') as file:
+                        text = file.read()
+                        self.text_editor.setText(text)
+            except FileNotFoundError:
+                pass
         elif action == 'Сохранить':
-            functionality.save_file(self)
+            title = self.text_title.text()
+            try:
+                if title != '':
+                    text = self.text_editor.toPlainText()
+                    file_name = f'{config.default_folder}/{title}'
+                    with open(file_name, 'x', encoding='utf-8') as file:
+                        file.write(text)
+                else:
+                    error = QMessageBox()
+                    error.setWindowTitle('Ошибка')
+                    error.setText('Пожалуйста, назовите Ваш текстовый документ')
+                    error.setIcon(QMessageBox.Warning)
+                    error.exec_()
+            except FileExistsError:
+                file_name = self.text_title.text()
+                text = self.text_editor.toPlainText()
+                with open(file_name, 'w', encoding='utf-8') as file:
+                    file.write(text)
         elif action == 'Сохранить как':
-            functionality.save_file_as(self)
+            file_name = QFileDialog.getSaveFileName(self, 'Сохранить файл', f'{config.default_folder}/{self.text_title.text()}', 'Текстовые файлы (*.txt)')[0]
+            text = self.text_editor.toPlainText()
+
+            try:
+                with open(file_name, 'w', encoding='utf-8') as file:
+                    file.write(text)
+            except FileNotFoundError:
+                pass
         elif action == 'Настройки':
-            functionality.settings()
+            settings_window = SettingsGUI()
+            settings_window.exec_()
         else:
             pass
 
