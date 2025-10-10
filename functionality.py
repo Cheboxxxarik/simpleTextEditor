1c683461876ffa2b202c2021c7902679d9f369fd
Тимур <tim.gai2009@yandex.ru>
2025-10-10 20:38:59 +0500

    Улучшена функция сохранения файлов.

--- functionality.py
+++ functionality.py
@@ -39,12 +39,13 @@ def save_file(self):
         try:
             if self.is_something_was_saved:
                 write_text(self, file_path=f'{config.default_folder}/{self.text_title.text()}', mode='w')
-            if title != '' and self.is_something_was_saved == False:
+            if title != '' and self.is_something_was_saved == False :
                 if title[-4:] != '.txt':
                     title = f'{title}.txt'
                 write_text(self, file_path=f'{config.default_folder}/{self.text_title.text()}', mode='x')
                 self.is_something_was_saved = True
-            if (title == '' or title == '.txt') and self.is_something_was_saved == False:
+                self.text_title.setReadOnly(True)
+            if title == '' and self.is_something_was_saved == False:
                 error_message(text='Пожалуйста, назовите Ваш текстовый документ')
         except FileExistsError:
             error_message(text='Файл с таким именем уже существует. Пожалуйста, переименуйте файл')
