900323011cbe4f8f3726cd3735525775d520277d
Тимур Гайсин <tim.gai2009@yandex.ru>
2025-12-24 19:50:52 +0500

    Changes to be committed: 	modified:   main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from PyQt6 import QtWidgets
 from PyQt6.QtGui import QIcon
-from PyQt6.QtCore import Qt
 from os.path import basename, splitext
 import config, config_stylesheet
 from settings_gui import SettingsGUI
